e978e7da21441e4b1897c55f5b70b4ceb7e18c0b
Nuno980627 <952571278@qq.com>
2022-04-13 14:41:42 +0800

    修改重复index.css名

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 //import './icons/index'
-import './styles/index.css'
+import './styles/tailwind.css'
 import './styles/index.scss'
 
 // import './utils/permission'
